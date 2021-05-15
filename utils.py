8edc70bb07315ca016f1dca19e68f3b4ea72814b
PotatoChipsNinja <shiyuhui2000@126.com>
2021-05-15 23:01:39 +0800

    fix bugs

--- utils.py
+++ utils.py
@@ -6,6 +6,39 @@ def add_inverse_rels(edge_index, rel):
     rel_all = torch.cat([rel, rel+rel.max()+1])
     return edge_index_all, rel_all
 
+'''
+def get_hits(encoder, decoder, triples, triple_dict, hits=(1, 3, 10)):
+    encoder.eval()
+    decoder.eval()
+    with torch.no_grad():
+        s, o, r = triples.t()
+        emb_ent = encoder()
+        pred = decoder(emb_ent, s, r)
+
+        # raw
+        _, idx = pred.sort(descending=True)
+        _, rank = idx.sort()
+        rank = rank.gather(dim=1, index=o.view(-1, 1)) + 1
+        print('Raw:\t', end='')
+        for k in hits:
+            print('Hits@%d: %.4f    ' % (k, (rank<=k).sum().item()/rank.size(0)), end='')
+        print('MRR: %.4f' % (1/rank).mean().item())
+
+        # filt.
+        for i, triple in enumerate(triples.tolist()):
+            if (triple[0], triple[2]) in triple_dict:
+                temp = pred[i][triple[1]].item()
+                pred[i][triple_dict[(triple[0], triple[2])]] = 0
+                pred[i][triple[1]] = temp
+        _, idx = pred.sort(descending=True)
+        _, rank = idx.sort()
+        rank = rank.gather(dim=1, index=o.view(-1, 1)) + 1
+        print('Filt.:\t', end='')
+        for k in hits:
+            print('Hits@%d: %.4f    ' % (k, (rank<=k).sum().item()/rank.size(0)), end='')
+        print('MRR: %.4f' % (1/rank).mean().item())
+'''
+
 def get_hits(encoder, decoder, triples, triple_dict, hits=(1, 3, 10), batch_size=128):
     encoder.eval()
     decoder.eval()
@@ -14,8 +47,8 @@ def get_hits(encoder, decoder, triples, triple_dict, hits=(1, 3, 10), batch_size
         emb_ent = encoder()
 
         batch_num = math.ceil(triples.size(0) / batch_size)
-        rank_raw = torch.tensor([], dtype=torch.long)
-        rank_filt = torch.tensor([], dtype=torch.long)
+        rank_raw = torch.tensor([], dtype=torch.long).to(triples.device)
+        rank_filt = torch.tensor([], dtype=torch.long).to(triples.device)
         for batch_id in range(batch_num):
             pred = decoder(emb_ent, s[batch_id*batch_size : (batch_id+1)*batch_size], r[batch_id*batch_size : (batch_id+1)*batch_size])
             # raw
