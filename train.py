2f83e1eaff2bebdb1226633e6496d7fde1c45062
PotatoChipsNinja <shiyuhui2000@126.com>
2021-05-16 08:49:15 +0800

    update

--- train.py
+++ train.py
@@ -14,8 +14,9 @@ def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--cuda", action="store_true", default=True)
     parser.add_argument("--data", choices=["WN18RR", "FB15k-237"])
-    parser.add_argument("--e_hidden", type=int, default=120)
-    parser.add_argument("--r_hidden", type=int, default=40)
+    parser.add_argument('--lr', type=float, default=0.001)
+    parser.add_argument("--e-hidden", type=int, default=120)
+    parser.add_argument("--r-hidden", type=int, default=40)
     parser.add_argument('--embedding-shape1', type=int, default=20)
     parser.add_argument('--hidden-drop', type=float, default=0.3)
     parser.add_argument('--input-drop', type=float, default=0.2)
@@ -23,8 +24,9 @@ def parse_args():
     parser.add_argument('--hidden-size', type=int, default=21888) # 注意这个随嵌入维度变化, 需要按照报错信息改
     parser.add_argument('--label-smoothing', type=float, default=0.1)
     parser.add_argument("--epoch", type=int, default=80)
-    parser.add_argument("--test_epoch", type=int, default=5)
-    parser.add_argument("--batch_size", type=int, default=2048)
+    parser.add_argument("--test-epoch", type=int, default=5)
+    parser.add_argument("--batch-size", type=int, default=2048)
+    parser.add_argument("--test-batch-size", type=int, default=128)
     args = parser.parse_args()
     return args
 
@@ -50,7 +52,7 @@ def main(args):
     data = MyData(args.data)[0].to(device)
     encoder = RAGA(args, data.ent_num).to(device)
     decoder = ConvE(args, data.ent_num, data.rel_num).to(device)
-    optimizer = torch.optim.Adam(itertools.chain(encoder.parameters(), decoder.parameters()))
+    optimizer = torch.optim.Adam(itertools.chain(encoder.parameters(), decoder.parameters()), lr=args.lr)
     criterion = nn.BCELoss()
 
     batch_num = math.ceil(data.train_set.size(0) / args.batch_size)
@@ -67,7 +69,7 @@ def main(args):
         avg_loss = torch.tensor(losses).mean().item()
         if (epoch+1)%args.test_epoch == 0:
             print()
-            get_hits(encoder, decoder, data.test_set, data.triple_dict)
+            get_hits(encoder, decoder, data.test_set, data.triple_dict, batch_size=args.test_batch_size)
 
 if __name__ == '__main__':
     args = parse_args()
